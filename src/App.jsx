cad60d1269082ab62df372b376507207386e0ef3
Jhersvin <jhersvinvillodasquinto@gmail.com>
2025-01-10 19:57:54 -0500

    No terminado NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
-import Footer from './components/Footer/Footer'
-
-import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
 import Main from './components/Main/Main'
+import Footer from './components/Footer/Footer'
 function App() {
   return (
     <>
-    <Header />
+    <Navbar />
     <Main/>
     <Footer />
     </>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './Navbar.css'
+import NumNum from '../../resources/icons/NumNum.svg'
 import Instagram from '../../resources/icons/Instagram.svg'
 import Youtube from '../../resources/icons/Youtube.svg'
 import Ex from '../../resources/icons/X.svg'
 function Navbar() {
   return (
-    <nav>
+    <header>
+    <nav className='NavBar'>
+      {/* Logo Icono */}
+      <img className='LogoHeader' src={NumNum} alt="Logo DigitalRescue" height="60px" />
       {/* Opciones */}
       <ul className='Opciones'>
           <li><a href="HomePage">Home</a></li>
           <li><a href="Recetario">Recetario</a></li>
-            <li><a href="Pasteles">Pasteles</a></li>
+          <li><a href="Pasteles">Pasteles</a></li>
           <li><a href="Nosotros">Nosotros</a></li>
       </ul>
       {/*Redes*/}
@@ -20,6 +24,7 @@ function Navbar() {
         <li className='Red'><a href="#"><img src={Ex} alt="Tiktok" /></a></li>
       </ul>
     </nav>
+    </header>
   )
 }
 
