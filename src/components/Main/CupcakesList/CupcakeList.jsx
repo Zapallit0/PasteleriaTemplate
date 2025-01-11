7f5cb49ff3675be7778f739b8bb7ed3953894820
Jhersvin <jhersvinvillodasquinto@gmail.com>
2025-01-11 18:48:23 -0500

    Inicio CupcakeList

--- src/components/Main/CupcakesList/CupcakeList.jsx
+++ src/components/Main/CupcakesList/CupcakeList.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './CupcakeList.css'
+
 // Cupcakes
 import BananaCupcake from '../../../resources/desserts/BananaCupcake.png'
 import ChocoChipsCupcake from '../../../resources/desserts/ChocoChipsCupcake.png'
@@ -6,27 +8,29 @@ import ChocolateCupcake from '../../../resources/desserts/ChocolateCupcake.png'
 import VanillaCupcake from '../../../resources/desserts/VanillaCupcake.png'
 function CupcakeList() {
   return (
-    <section className='CupcakesCourusel'>
+    <section className='CupcakesList'>
               <h3>!Lo más fresco¡</h3>
-              <div>
-                <div><img src={BananaCupcake} alt="Banana Cupcake" height="200px"/></div>
-                <h5>Cupcake#1</h5>
-                <p>desde S/5</p>
-              </div>
-              <div>
-                <div><img src={VanillaCupcake} alt="Vanilla Cupcake" height="200px"/></div>
-                <h5>Cupcake#2</h5>
-                <p>desde S/6</p>
-              </div>
-              <div>
-                <div><img src={ChocolateCupcake} alt="Chocolate Cupcake" height="200px"/></div>
-                <h5>Cupcake#3</h5>
-                <p>desde S/7</p>
-              </div>
-              <div>
-                <div><img src={ChocoChipsCupcake} alt="Choco Chips Cupcake" height="200px"/></div>
-                <h5>Cupcake#4</h5>
-                <p>desde S/8</p>
+              <div className='Cupcakes'>
+                <div className='Cupcake'>
+                  <div><img src={BananaCupcake} alt="Banana Cupcake" height="200px"/></div>
+                  <h5>Cupcake#1</h5>
+                  <p>desde S/5</p>
+                </div>
+                <div className='Cupcake'>
+                  <div><img src={VanillaCupcake} alt="Vanilla Cupcake" height="200px"/></div>
+                  <h5>Cupcake#2</h5>
+                  <p>desde S/6</p>
+                </div>
+                <div className='Cupcake'>
+                  <div><img src={ChocolateCupcake} alt="Chocolate Cupcake" height="200px"/></div>
+                  <h5>Cupcake#3</h5>
+                  <p>desde S/7</p>
+                </div>
+                <div className='Cupcake'>
+                  <div><img src={ChocoChipsCupcake} alt="Choco Chips Cupcake" height="200px"/></div>
+                  <h5>Cupcake#4</h5>
+                  <p>desde S/8</p>
+                </div>
               </div>
               <button>
                 !Compra AQUI¡ 
